160a811fad9b425542844e18b2f9b82ba484043a
tubone24 <tubo.yyyuuu@gmail.com>
2020-07-26 19:56:57 +0900

    友だち追加

--- components/Header.vue
+++ components/Header.vue
@@ -5,15 +5,21 @@
     </v-card-title>
     <v-card-text>
       <p>
-        近くのラーメン屋を検索できるLINE BOT ～ラーメン食べたい～ のWebページです。<br />
+        近くのラーメン屋を検索できるLINE BOT<br />
+        <b>ラーメン食べたい</b>のWebページです。<br />
+        下記のボタンからLINEBOTをフォローしよう！
       </p>
       <hr class="my-3" />
     </v-card-text>
     <v-card-actions>
       <v-spacer />
-      <v-btn color="primary" @click="login">
-        Twitterでログイン
-      </v-btn>
+      <a href="https://lin.ee/Y9mUrIN"
+        ><img
+          src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png"
+          alt="友だち追加"
+          height="36"
+          border="0"
+      /></a>
     </v-card-actions>
   </v-card>
 </template>
