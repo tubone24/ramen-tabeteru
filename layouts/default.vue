2e51fae0c2d9ecc83ae581c1c39fe01706197922
tubone(Yu Otsubo) <tubo.yyyuuu@gmail.com>
2022-09-11 16:25:58 +0900

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -36,20 +36,6 @@
       </v-container>
     </v-content>
     <v-bottom-navigation v-model="bottomNav">
-      <v-btn value="recent">
-        <span>Recent</span>
-        <v-icon>mdi-history</v-icon>
-      </v-btn>
-
-      <v-btn value="favorites">
-        <span>Favorites</span>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn value="nearby">
-        <span>Nearby</span>
-        <v-icon>mdi-map-marker</v-icon>
-      </v-btn>
       <v-footer app>
         <span
           >&copy; {{ new Date().getFullYear() }}
